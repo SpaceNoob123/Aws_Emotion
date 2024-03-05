72d271ad398353bd71a3539ebe4260c411144bf8
SpaceNoob123 <markusgame31@gmail.com>
2024-03-05 20:09:49 +0200

    release

---
+++ Rekognition-Emotion/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Rekognition_Emotion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtImageUrl = new System.Windows.Forms.TextBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.lblResults = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Url";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnLoadFromUrl_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Folder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnSelectImage_Click);
+            // 
+            // txtImageUrl
+            // 
+            this.txtImageUrl.Location = new System.Drawing.Point(93, 15);
+            this.txtImageUrl.Name = "txtImageUrl";
+            this.txtImageUrl.Size = new System.Drawing.Size(630, 20);
+            this.txtImageUrl.TabIndex = 2;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(93, 41);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(630, 308);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 3;
+            this.pictureBox.TabStop = false;
+            // 
+            // lblResults
+            // 
+            this.lblResults.AutoSize = true;
+            this.lblResults.Location = new System.Drawing.Point(90, 352);
+            this.lblResults.Name = "lblResults";
+            this.lblResults.Size = new System.Drawing.Size(35, 13);
+            this.lblResults.TabIndex = 4;
+            this.lblResults.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 411);
+            this.Controls.Add(this.lblResults);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.txtImageUrl);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "EMOTION AI";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtImageUrl;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label lblResults;
+    }
+}
+
---
+++ Rekognition-Emotion/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Amazon.Rekognition;
+using Amazon.Rekognition.Model;
+using Image = Amazon.Rekognition.Model.Image;
+
+namespace Rekognition_Emotion
+{
+    public partial class Form1 : Form
+    {
+        private AmazonRekognitionClient rekognitionClient;
+        public Form1()
+        {
+            InitializeComponent();
+            rekognitionClient = new AmazonRekognitionClient(Amazon.RegionEndpoint.EUWest1);
+        }
+        private void btnSelectImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "PNG files (*.png)|*.png";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox.ImageLocation = openFileDialog.FileName;
+                AnalyzeEmotions(openFileDialog.FileName);
+            }
+        }
+
+        private void btnLoadFromUrl_Click(object sender, EventArgs e)
+        {
+            string imageUrl = txtImageUrl.Text;
+            using (WebClient webClient = new WebClient())
+            {
+                byte[] imageBytes = webClient.DownloadData(imageUrl);
+                using (MemoryStream ms = new MemoryStream(imageBytes))
+                {
+                    pictureBox.Image = System.Drawing.Image.FromStream(ms);
+                }
+
+                AnalyzeEmotions(imageBytes);
+            }
+        }
+
+        private void AnalyzeEmotions(string imagePath)
+        {
+            using (FileStream fs = new FileStream(imagePath, FileMode.Open, FileAccess.Read))
+            {
+                byte[] imageBytes = new byte[fs.Length];
+                fs.Read(imageBytes, 0, (int)fs.Length);
+                AnalyzeEmotions(imageBytes);
+            }
+        }
+
+        private void AnalyzeEmotions(byte[] imageBytes)
+        {
+            DetectFacesRequest detectFacesRequest = new DetectFacesRequest()
+            {
+                Image = new Image() { Bytes = new MemoryStream(imageBytes) },
+                Attributes = new List<string> { "ALL" }
+            };
+
+            try
+            {
+                DetectFacesResponse detectFacesResponse = rekognitionClient.DetectFaces(detectFacesRequest);
+                foreach (FaceDetail face in detectFacesResponse.FaceDetails)
+                {
+                    if (face.Emotions.Count > 0)
+                    {
+                        var dominantEmotion = face.Emotions.OrderByDescending(e => e.Confidence).FirstOrDefault();
+
+                        if (dominantEmotion != null)
+                        {
+                            lblResults.Invoke(new Action(() => {
+                                lblResults.Text = $"{dominantEmotion.Type} : {dominantEmotion.Confidence:F2}%";
+                            }));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error detecting faces: {ex.Message}");
+            }
+        }
+    }
+}
